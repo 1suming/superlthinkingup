4d9323e1607fed61d79c184376816782d241a5ff
skysunshine <1531430343@qq.com>
2024-12-11 23:36:55 +0800

    article list baseusercard页面去掉reputation显示

--- ui/src/components/Article/ArticleList/index.tsx
+++ ui/src/components/Article/ArticleList/index.tsx
@@ -179,6 +179,7 @@ const ArticleList: FC<Props> = ({
                     <BaseUserCard
                       data={li.operator}
                       showAvatar={false}
+                      showReputation={false}
                       className="me-1"
                     />
                     •
